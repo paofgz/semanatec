7ecff5bce57df063489015f2d6c654f68dcf5642
Paola Fernandez <paolafdezgz@gmail.com>
2021-10-27 19:24:50 -0500

    new test

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,8 +1,9 @@
 describe('Visit Home screen', () => {
     it('successfully loads and writes in the name field', () => {
       cy.visit('/')
-        cy.get('#nombre').type("Andres")
-        cy.get('#apellido').type("Torres")
+        cy.get('#nombre').type("Paola")
+        cy.get('#apellido').type("Fernandez")
         cy.get('#botonAgregar').click()
+        cy.get('#titulo').should('have.value', 'Hola Paola Fernandez, presionaste 1 veces el botón')
     })
   })
